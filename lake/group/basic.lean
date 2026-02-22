56eee16206eefc3e88e2bd54debcfd2c3efaaef0
Matt Currier <fingerskier@gmail.com>
2026-02-21 22:59:31 -0600

    Create basic.lean

---
+++ lake/group/basic.lean
@@ -0,0 +1,16 @@
+-- MattLib/Algebra/Group.lean
+namespace MattLib.Algebra
+
+-- Define what a group IS
+
+-- 19. Right identity follows from left identity + inverse
+-- (this is a real algebra textbook exercise)
+
+-- 20. Right inverse follows too
+
+-- 21. Inverse of inverse
+
+-- 22. Inverse of product: (ab)⁻¹ = b⁻¹a⁻¹
+
+
+end MattLib.Algebra
