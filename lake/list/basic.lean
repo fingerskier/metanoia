b7f8c616575cf16a7bcc7046b84d02af933be359
Matt Currier <fingerskier@gmail.com>
2026-02-21 22:58:08 -0600

    Add basic list operations in MattLib.List namespace

---
+++ lake/list/basic.lean
@@ -0,0 +1,12 @@
+-- MattLib/List/Basic.lean  
+namespace MattLib.List
+
+-- 15. Length of append
+
+-- 16. Reverse reverse = identity
+
+-- 17. Map preserves length
+
+-- 18. Filter only shrinks
+
+end MattLib.List
